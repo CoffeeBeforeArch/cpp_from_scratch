71054bc3702fa897ab94b55d0f6d159c34ce1a9b
Nick <coffeebeforearch@gmail.com>
2022-11-11 09:42:40 -0600

    revert to last working version of example

--- src/008_function_overloading/overloading.cpp
+++ src/008_function_overloading/overloading.cpp
@@ -8,13 +8,18 @@ void print_array(std::array<int, 3> array) {
     std::cout << '\n';
 }
 
-void print_array(std::array<int, 3> array) {
-    std::cout << "Hello, world!\n";
+void print_array(std::array<float, 3> array) {
+    for (auto value : array) {
+        std::cout << value << ' ';
+    }
+    std::cout << '\n';
 }
-
 
 int main() {
     std::array<int, 3> my_int_array = {1, 2, 3};
     print_array(my_int_array);
+    
+    std::array<float, 3> my_float_array = {1.1f, 2.2f, 3.3f};
+    print_array(my_float_array);
     return 0;
 }
